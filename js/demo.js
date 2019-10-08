7b5a8bb28f72e6f1697055b1e14ddc73b91e4e92
Levert <levertco@gmail.com>
2019-10-08 23:18:39 +0300

    updates

--- js/demo.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#7fcada',
-    lineColor: '#22a3c0'
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
